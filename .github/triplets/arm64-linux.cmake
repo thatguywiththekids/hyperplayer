2a88302400836f958ea246a184ce0d07c7020660
Johan Gill <johan.gill@bahnhof.se>
2026-07-12 14:19:45 +0200

    Only existing options, please.

--- .github/triplets/arm64-linux.cmake
+++ .github/triplets/arm64-linux.cmake
@@ -5,4 +5,4 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 # Disable platform-specific assembly optimizations in mpg123
 # to avoid linker and compilation errors on ARM64.
-list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DOPT_GENERIC=ON")
+list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DHAVE_ARCH_IS_ARM64=OFF")
